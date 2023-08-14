3ab80d09db1c58cd7514cbea65fd415f49982cce
Bruno Túlio <brunotulio.engcomp@gmail.com>
2023-08-14 06:50:11 -0300

    fix: gitignore

---
+++ internal/application/usecase/backup/backup_auto.go
@@ -0,0 +1,135 @@
+package backup
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	"github.com/BrunoTulio/GoDump/internal/domain"
+	"github.com/BrunoTulio/GoDump/internal/service/alert_dump"
+	"github.com/BrunoTulio/GoDump/internal/service/backup_files"
+	"github.com/BrunoTulio/GoDump/internal/service/configuration_file"
+	"github.com/BrunoTulio/GoDump/internal/service/drive"
+
+	"github.com/BrunoTulio/GoDump/internal/service/process_dump"
+
+	"github.com/BrunoTulio/GoDump/pkg/logger"
+	"github.com/robfig/cron/v3"
+	"golang.org/x/sync/errgroup"
+)
+
+type (
+	BackupAutoUseCase interface {
+		Generate(ctx context.Context)
+	}
+
+	backupAutoUseCase struct {
+		configurationFileService configuration_file.ConfigurationFileService
+		processDumpService       process_dump.ProcessDumpService
+		alertDumpService         alert_dump.AlertDumpService
+		driveService             drive.DriveService
+		backupsFilesService      backup_files.BackupFilesService
+		cron                     *cron.Cron
+	}
+)
+
+// Execute implements BackupGenerateUseCase.
+func (b *backupAutoUseCase) Generate(ctx context.Context) {
+
+	config, err := b.configurationFileService.Get()
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	err = config.IsValid()
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+	sort.Sort(config.Dumps)
+
+	b.cron.AddFunc(config.Cron, func() {
+		logger.Info("Initialize event backup database file")
+
+		group := errgroup.Group{}
+		for _, dump := range config.Dumps {
+			dump := dump
+
+			group.Go(func() error {
+
+				b.backupsFilesService.RemoveByDuration(config.DurationFileInFolder, dump)
+
+				pathFile, err := b.processDumpService.Backup(dump)
+
+				if err != nil {
+					b.notifyError(config, err)
+					return err
+				}
+
+				b.notifySuccess(config, fmt.Sprintf("Sucesso backup dump service path file %s", pathFile))
+
+				if !config.GoogleDrive.Enable {
+					return nil
+				}
+
+				err = b.driveService.Upload(context.Background(), pathFile, dump)
+
+				if err != nil {
+					b.notifyError(config, err)
+					return err
+				}
+
+				b.notifySuccess(config, "Sucesso backup dump service google drive")
+				return nil
+			})
+
+			err := group.Wait()
+
+			if err != nil {
+				logger.Error(err)
+			}
+		}
+	})
+
+}
+
+func (b *backupAutoUseCase) notifyError(config domain.Config, err error) {
+	if !config.Alert.Enable {
+		return
+	}
+
+	b.alertDumpService.Execute(alert_dump.AlertCommand{
+		Message: fmt.Sprintf("Error GoDump %v", err),
+		Mail:    config.Alert.SendMail,
+	})
+}
+
+func (b *backupAutoUseCase) notifySuccess(config domain.Config, msg string) {
+	if !config.Alert.Enable {
+		return
+	}
+
+	b.alertDumpService.Execute(alert_dump.AlertCommand{
+		Message: msg,
+		Mail:    config.Alert.SendMail,
+	})
+}
+
+func NewBackupAuto(
+	configurationFileService configuration_file.ConfigurationFileService,
+	processDumpService process_dump.ProcessDumpService,
+	alertDumpService alert_dump.AlertDumpService,
+	driveService drive.DriveService,
+	backupsFilesService backup_files.BackupFilesService,
+	cron *cron.Cron,
+) BackupAutoUseCase {
+	return &backupAutoUseCase{
+		configurationFileService,
+		processDumpService,
+		alertDumpService,
+		driveService,
+		backupsFilesService,
+		cron,
+	}
+}
---
+++ internal/application/usecase/backup/backup_maunal.go
@@ -0,0 +1,106 @@
+package backup
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/BrunoTulio/GoDump/internal/domain"
+
+	"github.com/BrunoTulio/GoDump/internal/service/configuration_file"
+	"github.com/BrunoTulio/GoDump/internal/service/process_dump"
+	"golang.org/x/sync/errgroup"
+)
+
+type (
+	BackupUseCase interface {
+		Generate() error
+		GenerateByKey(key string) error
+	}
+	backupUseCase struct {
+		configurationFileService configuration_file.ConfigurationFileService
+		processDumpService       process_dump.ProcessDumpService
+	}
+)
+
+// Generate implements BackupUseCase.
+func (b *backupUseCase) Generate() error {
+
+	config, err := b.configurationFileService.Get()
+
+	if err != nil {
+		return err
+	}
+
+	err = config.IsValid()
+
+	if err != nil {
+		return err
+	}
+
+	sort.Sort(config.Dumps)
+	group := errgroup.Group{}
+
+	for _, dump := range config.Dumps {
+		dump := dump
+		group.Go(func() error {
+			_, err := b.processDumpService.Backup(dump)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+	}
+
+	return group.Wait()
+
+}
+
+// GenerateByKey implements BackupUseCase.
+func (b *backupUseCase) GenerateByKey(key string) error {
+
+	config, err := b.configurationFileService.Get()
+
+	if err != nil {
+		return err
+	}
+
+	err = config.IsValid()
+
+	if err != nil {
+		return err
+	}
+
+	var dump domain.Dump
+
+	for _, d := range config.Dumps {
+		if key == d.Key {
+			dump = d
+		}
+	}
+
+	if dump.Key == "" {
+		return fmt.Errorf("Dump key [%s] not found", key)
+	}
+	group := errgroup.Group{}
+	group.Go(func() error {
+		_, err := b.processDumpService.Backup(dump)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return group.Wait()
+
+}
+
+func NewBackup(
+	configurationFileService configuration_file.ConfigurationFileService,
+	processDumpService process_dump.ProcessDumpService,
+) BackupUseCase {
+	return &backupUseCase{
+		configurationFileService,
+		processDumpService,
+	}
+}
